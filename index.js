e225fbb35f30de304e49fffe562aafac21317639
Joseph <wamiti8711@gmail.com>
2022-11-22 14:46:05 -0500

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+//multiplication
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+//Generate a random integer use Math.random()
+//number between 0 - 0.999
+// Multiply by 10 to give us a number between 0 and 9.999
+
+//Create a variable called random that will 
+//generate a random integer greater than 0
+//To check if the number is an integer use Number.isINteger()
+
+let random = Number.isInteger(Math.random() * 10) + 1;
+
+//Create a variable called mod set to an equation that will 
+//calculate the remainder of dividing variable num3 by num4; 
+//the remainder should be 4.
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3%num4;
+
+//Create a variable called max that finds the highest 
+//number in a set; the value returned should be 20
+
+// To find the maximum number we use Math.max()
+
+let max = Math.max(12,6,7,4,15,17,19,20)
+max;
